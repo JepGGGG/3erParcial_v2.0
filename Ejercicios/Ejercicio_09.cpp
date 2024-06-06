ec5b6bfb594157f436ee54e58fd1280f603412de
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-06 11:27:47 -0600

    Create Ejercicio_09.cpp

---
+++ Ejercicios/Ejercicio_09.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	int num,i;
+	cout<<"dime cuantos numeros quieres" << endl;
+	cin>>num;
+	for(i=1;i<=num;i++){
+	 cout<< i*5 << endl; 
+	}
+	return 0;
+}
