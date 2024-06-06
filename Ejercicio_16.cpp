f22dff91320944eb6f0b73f0840e6728c45548a9
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-06 12:02:35 -0600

    Create Ejercicio_16.cpp

---
+++ Ejercicio_16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int num,menor;
+
+cout<<"ingrese 5 numeros" << endl;
+	cin>>num;
+
+	for(int i=1;i<5;i++){
+		cin>>num;
+		if(num<menor){
+			menor=num;
+		}
+	}
+	cout<<"el numero menor es: "<<menor<<endl;
+
+	return 0;
+}
