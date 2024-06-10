54255ac1e03de56650362d241328fdb031c68d36
Jesús Efrain Pluma Garcia (JepG) <168058829+JepGGGG@users.noreply.github.com>
2024-06-10 12:24:09 -0600

    Create Ejercicio_55.cpp

---
+++ Ejercicios/Ejercicio_55.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+  string admin[20];
+  string user[20];
+  string anonimo[20];
+
+  stirng name[20];//nombe anonimo
+  string root[] = "admin";
+  string pass_ad[] = "1234";//esta es la contraseañ del admin
+  string user,user_password;
+  string user_identificador[]=user;
+  string pass_usuario[]=user_password;
+
+  
+  cout<<"ingresa tu tipo de usuario";
+  switch(int type){
+    cin >> type;
+    case 1://usuario
+      cout<<"ingresa tu nombre de usuario";
+      cin <<user
+      
+      
+  
+  cin>>;
+  cout<<"Ingresa tu contraseña: ";
+  cin>>;
+  if ((strmpc(user,admin)==0)&&(strcmp(pass,1234)){
+    cout<<"Bienvenido Administrador "<<endl
+    cout<<"Tienes las siguientes ventajas: "<<endl;
+    cout<<"1.Modificar mis datos personales\n";
+    cout<<"2.Mostrar mis vendedores recomendados\n";
+    cout<<"3.Recomendar a un vendedor\n";
+    cout<<"4.Mostrar todos los "
+    
+  }
+
+  if(strmpc(user,anonimo)==0){
+   cout<<"Ingresaste como anonimo";
+    cout<<"ingresa tu nombre en el que quieres acceder: ";
+    cin>>name;
+    cout<<"bienvenido al sistema "<<name;
+    cout<<"\n productos a la venta"
+    
+    
+  }
+  if(strcmp(user,user) == 0){
+    cout<<"Bienvenido Usuario "<<endl
+    cout<<"Bienvenido Administrador "<<endl
+    } 
+      }
+}
